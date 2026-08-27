85ce1188b228ad61a1c4e1bfbfe81245b0842439
Shiro <shiroshi1711@gmail.com>
2026-08-27 20:38:00 +0700

    chore: fix bugs

--- fetch.py
+++ fetch.py
@@ -2,7 +2,8 @@ from datetime import datetime as dt, timedelta
 from config import API_URL
 import requests
 
-def fetch_data(date):
+def fetch_data():
+    date = str(dt.today().date() - timedelta(days=1))
     url = API_URL+date
     response = requests.post(url)
     if response.status_code == 200:
